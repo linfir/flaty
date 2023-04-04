48807bbfaafde1758fa6d32b0f1f6230829fd53a
Yann Villessuzanne <yann@miomio.fr>
2023-04-04 09:41:20 +0200

    Hello, world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{
+    response::{IntoResponse, Response},
+    Router,
+};
+use tracing::log::info;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let address: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+
+    let app = Router::new().fallback(handler);
+
+    info!("Listening on http://{}", address);
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Response {
+    "Hello, world!".into_response()
 }

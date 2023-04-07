ec2041293513a55fc31d6e1b4b0ea6d75673b9cd
Yann Villessuzanne <yann@miomio.fr>
2023-04-07 14:13:12 +0200

    Caching

--- src/cache.rs
+++ src/cache.rs
@@ -1,23 +1,28 @@
-use std::{
-    io::{self, Read},
-    os::unix::prelude::MetadataExt,
-    path::Path,
-};
+use std::{io, os::unix::prelude::MetadataExt, path::Path};
 
 use tokio::{fs::File, io::AsyncReadExt};
 use twox_hash::xxh3::hash128;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Digest {
     mtime: i64,
     size: u64,
     hash: u128,
 }
 
-pub async fn try_reload(
-    path: &Path,
-    digest: Digest,
-) -> Result<Option<(Digest, String)>, io::Error> {
+pub async fn load(path: &Path) -> io::Result<(Digest, String)> {
+    let mut file = File::open(path).await?;
+    let meta = file.metadata().await?;
+    let size = meta.size();
+    let mtime = meta.mtime();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await?;
+    let hash = hash128(contents.as_bytes());
+    let digest = Digest { mtime, size, hash };
+    Ok((digest, contents))
+}
+
+pub async fn reload(path: &Path, digest: Digest) -> io::Result<Option<(Digest, String)>> {
     let mut file = File::open(path).await?;
     let meta = file.metadata().await?;
 
